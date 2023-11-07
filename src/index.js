49251fd00266e188e7f00d4493a677e444c32d44
Tanvi Naik <naiktanvi30@gmail.com>
2023-11-07 14:29:18 +0530

    ready to deploy

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from "react-dom";
 import Routers from "./Routers"
 import "./style.css"
 import 'mapbox-gl/dist/mapbox-gl.css';
+import {disableReactDevtools} from "@fvilers/disable-react-devtools";
 
+if (process.env.NODE_ENV === 'production') disableReactDevtools()
 ReactDOM.render(<Routers />, document.getElementById("root"));
